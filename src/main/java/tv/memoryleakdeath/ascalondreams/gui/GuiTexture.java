dcd85bc61fa09597a9a04f50bff799b3d34341c1
memory leak <memoryleakdeath@gmail.com>
2025-12-26 00:09:37 -0500

    Chapter 12 started

---
+++ src/main/java/tv/memoryleakdeath/ascalondreams/gui/GuiTexture.java
@@ -0,0 +1,18 @@
+package tv.memoryleakdeath.ascalondreams.gui;
+
+import java.security.SecureRandom;
+
+public record GuiTexture(long id, String texturePath) {
+   public GuiTexture(String texturePath) {
+      this(generateId(), texturePath);
+   }
+
+   private static long generateId() {
+      SecureRandom rand = new SecureRandom();
+      long id = Math.abs(rand.nextLong());
+      if(id == 0) {
+         id += 1;
+      }
+      return id;
+   }
+}
