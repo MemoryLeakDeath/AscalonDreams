82e3fe1e3ab76af778105d2523e46b2b20a85add
memory leak <memoryleakdeath@gmail.com>
2026-01-10 20:35:47 -0500

    - initial stab at AO

--- src/main/java/tv/memoryleakdeath/ascalondreams/render/ssao/AmbientOcclusionRenderer.java
+++ src/main/java/tv/memoryleakdeath/ascalondreams/render/ssao/AmbientOcclusionRenderer.java
@@ -5,13 +5,18 @@ import org.lwjgl.system.MemoryUtil;
 import org.lwjgl.util.shaderc.Shaderc;
 import org.lwjgl.vulkan.VK13;
 import org.lwjgl.vulkan.VkAttachmentDescription;
+import org.lwjgl.vulkan.VkAttachmentReference;
 import org.lwjgl.vulkan.VkClearValue;
 import org.lwjgl.vulkan.VkCommandBuffer;
 import org.lwjgl.vulkan.VkExtent2D;
+import org.lwjgl.vulkan.VkFramebufferCreateInfo;
 import org.lwjgl.vulkan.VkRect2D;
 import org.lwjgl.vulkan.VkRenderPassCreateInfo;
 import org.lwjgl.vulkan.VkRenderingAttachmentInfo;
 import org.lwjgl.vulkan.VkRenderingInfo;
+import org.lwjgl.vulkan.VkSamplerCreateInfo;
+import org.lwjgl.vulkan.VkSubpassDependency;
+import org.lwjgl.vulkan.VkSubpassDescription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tv.memoryleakdeath.ascalondreams.buffers.GlobalBuffers;
@@ -69,11 +74,11 @@ public class AmbientOcclusionRenderer implements Renderer {
 //   private static final String VERTEX_SHADER_FILE_GLSL = "shaders/shadow_vertex_shader.glsl";
 //   private static final String VERTEX_SHADER_FILE_SPV = VERTEX_SHADER_FILE_GLSL + ".spv";
    private static final List<ShaderInfo> SHADERS = List.of(
-           new ShaderInfo("shaders/ssao/blur_fragment.glsl", VulkanConstants.FRAGMENT_SHADER_TYPE, true),
-           new ShaderInfo("shaders/ssao/composition_fragment.glsl", VulkanConstants.FRAGMENT_SHADER_TYPE, true),
-           new ShaderInfo("shaders/ssao/fullscreen_vertex.glsl", VulkanConstants.VERTEX_SHADER_TYPE, true),
-           new ShaderInfo("shaders/ssao/gbuffer_fragment.glsl", VulkanConstants.FRAGMENT_SHADER_TYPE, true),
-           new ShaderInfo("shaders/ssao/gbuffer_vertex.glsl", VulkanConstants.VERTEX_SHADER_TYPE, true)
+           new ShaderInfo("shaders/ssao/blur_fragment.glsl", VulkanConstants.FRAGMENT_SHADER_TYPE, VK13.VK_SHADER_STAGE_FRAGMENT_BIT, true),
+           new ShaderInfo("shaders/ssao/composition_fragment.glsl", VulkanConstants.FRAGMENT_SHADER_TYPE, VK13.VK_SHADER_STAGE_FRAGMENT_BIT, true),
+           new ShaderInfo("shaders/ssao/fullscreen_vertex.glsl", VulkanConstants.VERTEX_SHADER_TYPE, VK13.VK_SHADER_STAGE_VERTEX_BIT, true),
+           new ShaderInfo("shaders/ssao/gbuffer_fragment.glsl", VulkanConstants.FRAGMENT_SHADER_TYPE, VK13.VK_SHADER_STAGE_FRAGMENT_BIT, true),
+           new ShaderInfo("shaders/ssao/gbuffer_vertex.glsl", VulkanConstants.VERTEX_SHADER_TYPE, VK13.VK_SHADER_STAGE_VERTEX_BIT, true)
    );
 
 //   private final List<CascadeShadows> cascadeShadows;
@@ -102,6 +107,15 @@ public class AmbientOcclusionRenderer implements Renderer {
    private Attachment ssaoBlurAttachment;
    private VkRenderingAttachmentInfo.Buffer ssaoBlurAttachmentInfo;
    private VkAttachmentDescription.Buffer attachmentDescriptions;
+   private VkSubpassDescription subpassDescription;
+   private VkSubpassDependency.Buffer subpassDependencies;
+   private long gBufferRenderPass;
+   private long gBufferFrameBuffer;
+   private long ssaoRenderPass;
+   private long ssaoFrameBuffer;
+   private long ssaoBlurRenderPass;
+   private long ssaoBlurFrameBuffer;
+   private long ssaoSampler;
 
 
    // singletons
@@ -125,7 +139,13 @@ public class AmbientOcclusionRenderer implements Renderer {
       this.ssaoColorAttachmentInfo = initSSAOColorAttachmentInfo(ssaoColorAttachment, clearColor);
       this.ssaoBlurAttachment = initSSAOAttachment(device, allocationUtil, swapChain);
       this.ssaoBlurAttachmentInfo = initSSAOColorAttachmentInfo(ssaoBlurAttachment, clearColor);
-      this.attachmentDescriptions = initAttachmentDescriptions(materialAttachments);
+      this.gBufferRenderPass = createGBufferRenderPass(device, materialAttachments);
+      this.gBufferFrameBuffer = createGBufferFrameBuffer(device, materialAttachments, gBufferRenderPass);
+      this.ssaoRenderPass = createSSAORenderPass(device, ssaoColorAttachment);
+      this.ssaoFrameBuffer = createSSAOFrameBuffer(device, ssaoColorAttachment, ssaoRenderPass);
+      this.ssaoBlurRenderPass = createSSAOBlurRenderPass(device, ssaoBlurAttachment);
+      this.ssaoBlurFrameBuffer = createSSAOBlurFrameBuffer(device, ssaoBlurAttachment, ssaoBlurRenderPass);
+      this.ssaoSampler = createSampler(device);
 
 //      depthAttachment = initDepthAttachment(device, allocationUtil);
 //      this.depthAttachmentInfo = initDepthAttachmentInfo(depthAttachment, clearDepth);
@@ -201,10 +221,11 @@ public class AmbientOcclusionRenderer implements Renderer {
               .clearValue(clearValue);
    }
 
-   private static VkAttachmentDescription.Buffer initAttachmentDescriptions(MaterialAttachments attachments) {
-      try(var stack = MemoryStack.stackPush()) {
+   private static long createGBufferRenderPass(LogicalDevice device, MaterialAttachments attachments) {
+      LongBuffer renderPassBuf = MemoryUtil.memAllocLong(1);
+      try (var stack = MemoryStack.stackPush()) {
          var attachmentDescriptionBuffer = VkAttachmentDescription.calloc(attachments.getColorAttachments().size(), stack);
-         for(int i = 0; i < attachments.getColorAttachments().size(); i++) {
+         for (int i = 0; i < attachments.getColorAttachments().size(); i++) {
             var colorAttachment = attachments.getColorAttachments().get(i);
             attachmentDescriptionBuffer.get(i)
                     .samples(VK13.VK_SAMPLE_COUNT_1_BIT)
@@ -215,16 +236,231 @@ public class AmbientOcclusionRenderer implements Renderer {
                     .finalLayout(colorAttachment.isDepthAttachment() ?
                             VK13.VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL : VK13.VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
                     .format(colorAttachment.getImage().getFormat());
+
+            int colorAttachmentSize = attachments.getColorAttachments().size();
+            var attachmentReferences = VkAttachmentReference.calloc(colorAttachmentSize, stack);
+            for (int i = 0; i < colorAttachmentSize; i++) {
+               attachmentReferences.get(i)
+                       .attachment(i)
+                       .layout(VK13.VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+            }
+            var depthAttachmentReference = VkAttachmentReference.calloc(1, stack)
+                    .get()
+                    .attachment(3)
+                    .layout(VK13.VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
+            var subpassDescriptions = VkSubpassDescription.calloc(1, stack);
+            subpassDescriptions.get(0)
+                    .pipelineBindPoint(VK13.VK_PIPELINE_BIND_POINT_GRAPHICS)
+                    .pColorAttachments(attachmentReferences)
+                    .colorAttachmentCount(attachments.getColorAttachments().size())
+                    .pDepthStencilAttachment(depthAttachmentReference);
+
+            var subpassDependencies = VkSubpassDependency.calloc(3, stack);
+            subpassDependencies.get(0)
+                    .srcSubpass(VK13.VK_SUBPASS_EXTERNAL)
+                    .dstSubpass(0)
+                    .srcStageMask(VK13.VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK13.VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT)
+                    .dstStageMask(VK13.VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK13.VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT)
+                    .srcAccessMask(VK13.VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT)
+                    .dstAccessMask(VK13.VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT | VK13.VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT)
+                    .dependencyFlags(0);
+
+            subpassDependencies.get(1)
+                    .srcSubpass(VK13.VK_SUBPASS_EXTERNAL)
+                    .dstSubpass(0)
+                    .srcStageMask(VK13.VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT)
+                    .dstStageMask(VK13.VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)
+                    .srcAccessMask(VK13.VK_ACCESS_SHADER_READ_BIT)
+                    .dstAccessMask(VK13.VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT)
+                    .dependencyFlags(VK13.VK_DEPENDENCY_BY_REGION_BIT);
+
+            subpassDependencies.get(2)
+                    .srcSubpass(0)
+                    .dstSubpass(VK13.VK_SUBPASS_EXTERNAL)
+                    .srcStageMask(VK13.VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)
+                    .dstStageMask(VK13.VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT)
+                    .srcAccessMask(VK13.VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT)
+                    .dstAccessMask(VK13.VK_ACCESS_SHADER_READ_BIT)
+                    .dependencyFlags(VK13.VK_DEPENDENCY_BY_REGION_BIT);
+
+            var info = VkRenderPassCreateInfo.calloc()
+                    .sType$Default()
+                    .pAttachments(attachmentDescriptionBuffer)
+                    .pSubpasses(subpassDescriptions)
+                    .pDependencies(subpassDependencies);
+            VulkanUtils.failIfNeeded(VK13.vkCreateRenderPass(device.getDevice(), info, null, renderPassBuf), "Error creating g-buffer render pass!");
          }
-         return attachmentDescriptionBuffer;
+         return renderPassBuf.get(0);
       }
    }
 
-   private static VkRenderPassCreateInfo initRenderPass(DescriptorAllocator allocator) {
-      return VkRenderPassCreateInfo.calloc()
-              .sType$Default()
-              .pAttachments(allocator.getDescriptorSet("").);
+   private static long createGBufferFrameBuffer(LogicalDevice device, MaterialAttachments attachments, long gBufferRenderPass) {
+      LongBuffer attachmentBuffer = MemoryUtil.memAllocLong(attachments.getAllAttachments().size());
+      for(int i = 0; i < attachments.getAllAttachments().size(); i++) {
+         attachmentBuffer.put(i, attachments.getAllAttachments().get(i).getImageView().getImageViewId());
+      }
+      LongBuffer frameBuffer = MemoryUtil.memAllocLong(1);
+      var info = VkFramebufferCreateInfo.calloc()
+              .renderPass(gBufferRenderPass)
+              .pAttachments(attachmentBuffer)
+              .attachmentCount(attachments.getAllAttachments().size())
+              .width(attachments.getWidth())
+              .height(attachments.getHeight())
+              .layers(1);
+      VulkanUtils.failIfNeeded(VK13.vkCreateFramebuffer(device.getDevice(), info, null, frameBuffer), "Unable to create GBuffer Framebuffer!");
+      return frameBuffer.get(0);
    }
+
+   private static long createSSAORenderPass(LogicalDevice device, Attachment ssaoColorAttachment) {
+      LongBuffer renderPass = MemoryUtil.memAllocLong(1);
+      try(var stack = MemoryStack.stackPush()) {
+         var attachmentDescription = VkAttachmentDescription.calloc(1, stack);
+         attachmentDescription.get(0)
+                 .format(ssaoColorAttachment.getImage().getFormat())
+                 .samples(VK13.VK_SAMPLE_COUNT_1_BIT)
+                 .loadOp(VK13.VK_ATTACHMENT_LOAD_OP_CLEAR)
+                 .storeOp(VK13.VK_ATTACHMENT_STORE_OP_STORE)
+                 .stencilLoadOp(VK13.VK_ATTACHMENT_LOAD_OP_DONT_CARE)
+                 .stencilStoreOp(VK13.VK_ATTACHMENT_STORE_OP_DONT_CARE)
+                 .initialLayout(VK13.VK_IMAGE_LAYOUT_UNDEFINED)
+                 .finalLayout(VK13.VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+         var colorReference = VkAttachmentReference.calloc(1, stack);
+         colorReference.get(0)
+                 .layout(VK13.VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+
+         var subpassDescription = VkSubpassDescription.calloc(1, stack);
+         subpassDescription.get(0)
+                 .pipelineBindPoint(VK13.VK_PIPELINE_BIND_POINT_GRAPHICS)
+                 .pColorAttachments(colorReference)
+                 .colorAttachmentCount(1);
+
+         var subpassDependencies = VkSubpassDependency.calloc(2, stack);
+         subpassDependencies.get(0)
+                 .srcSubpass(VK13.VK_SUBPASS_EXTERNAL)
+                 .dstSubpass(0)
+                 .srcStageMask(VK13.VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT)
+                 .dstStageMask(VK13.VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)
+                 .srcAccessMask(VK13.VK_ACCESS_MEMORY_READ_BIT)
+                 .dstAccessMask(VK13.VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK13.VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT)
+                 .dependencyFlags(VK13.VK_DEPENDENCY_BY_REGION_BIT);
+         subpassDependencies.get(1)
+                 .srcSubpass(0)
+                 .dstSubpass(VK13.VK_SUBPASS_EXTERNAL)
+                 .srcStageMask(VK13.VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)
+                 .dstStageMask(VK13.VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT)
+                 .srcAccessMask(VK13.VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK13.VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT)
+                 .dstAccessMask(VK13.VK_ACCESS_MEMORY_READ_BIT)
+                 .dependencyFlags(VK13.VK_DEPENDENCY_BY_REGION_BIT);
+
+         var info = VkRenderPassCreateInfo.calloc()
+                 .sType$Default()
+                 .pAttachments(attachmentDescription)
+                 .pSubpasses(subpassDescription)
+                 .pDependencies(subpassDependencies);
+         VulkanUtils.failIfNeeded(VK13.vkCreateRenderPass(device.getDevice(), info, null, renderPass), "Failed to create SSAO render pass!");
+      }
+      return renderPass.get(0);
+   }
+
+   private static long createSSAOFrameBuffer(LogicalDevice device, Attachment ssaoColorAttachment, long ssaoRenderPass) {
+      LongBuffer colorAttachmentBuf = MemoryUtil.memAllocLong(1);
+      colorAttachmentBuf.put(0, ssaoColorAttachment.getImageView().getImageViewId());
+      LongBuffer frameBuffer = MemoryUtil.memAllocLong(1);
+      var info = VkFramebufferCreateInfo.calloc()
+              .renderPass(ssaoRenderPass)
+              .pAttachments(colorAttachmentBuf)
+              .attachmentCount(1)
+              .width(ssaoColorAttachment.getImage().getWidth())
+              .height(ssaoColorAttachment.getImage().getHeight())
+              .layers(1);
+      VulkanUtils.failIfNeeded(VK13.vkCreateFramebuffer(device.getDevice(), info, null, frameBuffer), "Unable to create SSAO Framebuffer!");
+      return frameBuffer.get(0);
+   }
+
+   private static long createSSAOBlurRenderPass(LogicalDevice device, Attachment ssaoBlurAttachment) {
+      LongBuffer renderPass = MemoryUtil.memAllocLong(1);
+      try(var stack = MemoryStack.stackPush()) {
+         var attachmentDescription = VkAttachmentDescription.calloc(1, stack);
+         attachmentDescription.get(0)
+                 .format(ssaoBlurAttachment.getImage().getFormat())
+                 .samples(VK13.VK_SAMPLE_COUNT_1_BIT)
+                 .loadOp(VK13.VK_ATTACHMENT_LOAD_OP_CLEAR)
+                 .storeOp(VK13.VK_ATTACHMENT_STORE_OP_STORE)
+                 .stencilLoadOp(VK13.VK_ATTACHMENT_LOAD_OP_DONT_CARE)
+                 .stencilStoreOp(VK13.VK_ATTACHMENT_STORE_OP_DONT_CARE)
+                 .initialLayout(VK13.VK_IMAGE_LAYOUT_UNDEFINED)
+                 .finalLayout(VK13.VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+         var colorReference = VkAttachmentReference.calloc(1, stack);
+         colorReference.get(0)
+                 .layout(VK13.VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+
+         var subpassDescription = VkSubpassDescription.calloc(1, stack);
+         subpassDescription.get(0)
+                 .pipelineBindPoint(VK13.VK_PIPELINE_BIND_POINT_GRAPHICS)
+                 .pColorAttachments(colorReference)
+                 .colorAttachmentCount(1);
+
+         var subpassDependencies = VkSubpassDependency.calloc(2, stack);
+         subpassDependencies.get(0)
+                 .srcSubpass(VK13.VK_SUBPASS_EXTERNAL)
+                 .dstSubpass(0)
+                 .srcStageMask(VK13.VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT)
+                 .dstStageMask(VK13.VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)
+                 .srcAccessMask(VK13.VK_ACCESS_MEMORY_READ_BIT)
+                 .dstAccessMask(VK13.VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK13.VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT)
+                 .dependencyFlags(VK13.VK_DEPENDENCY_BY_REGION_BIT);
+         subpassDependencies.get(1)
+                 .srcSubpass(0)
+                 .dstSubpass(VK13.VK_SUBPASS_EXTERNAL)
+                 .srcStageMask(VK13.VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)
+                 .dstStageMask(VK13.VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT)
+                 .srcAccessMask(VK13.VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK13.VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT)
+                 .dstAccessMask(VK13.VK_ACCESS_MEMORY_READ_BIT)
+                 .dependencyFlags(VK13.VK_DEPENDENCY_BY_REGION_BIT);
+
+         var info = VkRenderPassCreateInfo.calloc()
+                 .sType$Default()
+                 .pAttachments(attachmentDescription)
+                 .pSubpasses(subpassDescription)
+                 .pDependencies(subpassDependencies);
+         VulkanUtils.failIfNeeded(VK13.vkCreateRenderPass(device.getDevice(), info, null, renderPass), "Failed to create SSAOBlur render pass!");
+      }
+      return renderPass.get(0);
+   }
+
+   private static long createSSAOBlurFrameBuffer(LogicalDevice device, Attachment ssaoBlurAttachment, long ssaoBlurRenderPass) {
+      LongBuffer colorAttachmentBuf = MemoryUtil.memAllocLong(1);
+      colorAttachmentBuf.put(0, ssaoBlurAttachment.getImageView().getImageViewId());
+      LongBuffer frameBuffer = MemoryUtil.memAllocLong(1);
+      var info = VkFramebufferCreateInfo.calloc()
+              .renderPass(ssaoBlurRenderPass)
+              .pAttachments(colorAttachmentBuf)
+              .attachmentCount(1)
+              .width(ssaoBlurAttachment.getImage().getWidth())
+              .height(ssaoBlurAttachment.getImage().getHeight())
+              .layers(1);
+      VulkanUtils.failIfNeeded(VK13.vkCreateFramebuffer(device.getDevice(), info, null, frameBuffer), "Unable to create SSAOBlur Framebuffer!");
+      return frameBuffer.get(0);
+   }
+
+   private static long createSampler(LogicalDevice device) {
+      LongBuffer samplerBuf = MemoryUtil.memAllocLong(1);
+      var sampler = VkSamplerCreateInfo.calloc()
+              .magFilter(VK13.VK_FILTER_NEAREST)
+              .minFilter(VK13.VK_FILTER_NEAREST)
+              .mipmapMode(VK13.VK_SAMPLER_MIPMAP_MODE_LINEAR)
+              .addressModeU(VK13.VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE)
+              .addressModeV(VK13.VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE)
+              .addressModeW(VK13.VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE)
+              .mipLodBias(0f)
+              .maxAnisotropy(1f)
+              .minLod(0f)
+              .maxLod(1f)
+              .borderColor(VK13.VK_BORDER_COLOR_FLOAT_OPAQUE_WHITE);
+      VulkanUtils.failIfNeeded(VK13.vkCreateSampler(device.getDevice(), sampler, null, samplerBuf), "Failed to create SSAO sampler!");
+      return samplerBuf.get(0);
+   }
+
 
 //   private static Attachment initDepthAttachment(LogicalDevice device, MemoryAllocationUtil allocationUtil) {
 //      return new Attachment(device, allocationUtil, ShadowUtils.SHADOW_MAP_SIZE, ShadowUtils.SHADOW_MAP_SIZE,
@@ -270,11 +506,7 @@ public class AmbientOcclusionRenderer implements Renderer {
 
    private static List<ShaderModule> initShaderModules(LogicalDevice device) {
       ShaderCompiler.compileShadersIfChanged(SHADERS);
-      return SHADERS.stream().map(info -> new ShaderModule(device, ))
-
-      return List.of(new ShaderModule(device, VK13.VK_SHADER_STAGE_VERTEX_BIT, VERTEX_SHADER_FILE_SPV, null),
-              new ShaderModule(device, VK13.VK_SHADER_STAGE_GEOMETRY_BIT, SHADOW_GEOMETRY_SHADER_FILE_SPV, null),
-              new ShaderModule(device, VK13.VK_SHADER_STAGE_FRAGMENT_BIT, FRAGMENT_SHADER_FILE_SPV, null));
+      return SHADERS.stream().map(info -> info.getShaderModule(device, null)).toList();
    }
 
    @Override
--- src/main/java/tv/memoryleakdeath/ascalondreams/shaders/ShaderInfo.java
+++ src/main/java/tv/memoryleakdeath/ascalondreams/shaders/ShaderInfo.java
@@ -1,7 +1,14 @@
 package tv.memoryleakdeath.ascalondreams.shaders;
 
-public record ShaderInfo(String file, int type, boolean debug) {
+import org.lwjgl.vulkan.VkSpecializationInfo;
+import tv.memoryleakdeath.ascalondreams.device.LogicalDevice;
+
+public record ShaderInfo(String file, int shaderType, int shaderStage, boolean debug) {
    public String getSpvName() {
       return file + ".spv";
+   }
+
+   public ShaderModule getShaderModule(LogicalDevice device, VkSpecializationInfo info) {
+      return new ShaderModule(device, shaderStage, getSpvName(), info);
    }
 }
